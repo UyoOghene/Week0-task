65411ee5853c7b12497f026d9e8fded2f738f320
Uyooghene <uyooghene01@gmail.com>
2024-02-05 22:46:01 -0800

    feb 5 2024

--- app.js
+++ app.js
@@ -9,61 +9,7 @@
 //     'violet'
 // ];
 
- // Function to handle color clicks
- function handleColorClick(color) {
-    displayClickedColor(color);
-    highlightColorSquare(color);
-}
+const colorbox=document.getElementsByClassName('box')
+colorbox.addEventListener('click',function()
+{alert('hi');});
 
-// Function to display clicked color below the color box
-function displayClickedColor(color) {
-    const displayColor = document.getElementById('display-color');
-    displayColor.textContent = color;
-}
-
-// Function to highlight clicked color square
-function highlightColorSquare(color) {
-    const colorSquares = document.querySelectorAll('.color-square');
-    colorSquares.forEach(square => square.style.border = 'none');
-    const clickedSquare = document.getElementById(color);
-    clickedSquare.style.border = '2px solid black';
-}
-
-// Event listeners for color clicks
-document.getElementById('red').addEventListener('click', function () {
-    handleColorClick('red');
-});
-
-document.getElementById('blue').addEventListener('click', function () {
-    handleColorClick('blue');
-});
-
-document.getElementById('green').addEventListener('click', function () {
-    handleColorClick('green');
-});
-
-document.getElementById('yellow').addEventListener('click', function () {
-    handleColorClick('yellow');
-});
-
-document.getElementById('indigo').addEventListener('click', function () {
-    handleColorClick('indigo');
-});
-
-document.getElementById('violet').addEventListener('click', function () {
-    handleColorClick('violet');
-});
-
-
-// function displayClickedColor(color) {
-//     const displayColor = document.getElementById('display-color');
-//     displayColor.textContent = color;
-// }
-// // Function to highlight clicked color square
-// function highlightColorSquare(square) {
-//     const colorSquares = document.querySelectorAll('.color-square');
-//     colorSquares.forEach(square => square.classList.remove('clicked'));
-//     square.classList.add('clicked');
-// }
-
-// applyColors();
