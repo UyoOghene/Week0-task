44f6b92dd88565b93b28b48b90bfdcce06e755c2
Uyooghene <uyooghene01@gmail.com>
2024-02-06 00:25:38 -0800

    feb05

--- app.js
+++ app.js
@@ -9,7 +9,59 @@
 //     'violet'
 // ];
 
-const colorbox=document.getElementsByClassName('box')
-colorbox.addEventListener('click',function()
-{alert('hi');});
+
+const redbox = document.querySelectorAll('.red');
+redbox.forEach(function(box) {
+    box.addEventListener('click',function boxtext(){const txt= document.getElementById('colortext');
+    const content = '<p>red</p>';
+    txt.innerHTML=content;}
+);
+});
+
+const orangebox = document.querySelectorAll('.orange');
+orangebox.forEach(function(box) {
+    box.addEventListener('click', function boxtext(){const txt= document.getElementById('colortext');
+    const content = '<p>orange</p>';
+    txt.innerHTML=content;}
+);
+});
+
+const yellowbox = document.querySelectorAll('.yellow');
+yellowbox.forEach(function(box) {
+    box.addEventListener('click',function boxtext(){const txt= document.getElementById('colortext');
+    const content = '<p>yellow</p>';
+    txt.innerHTML=content;}
+);
+});
+
+const bluebox = document.querySelectorAll('.blue');
+bluebox.forEach(function(box) {
+    box.addEventListener('click', function boxtext(){const txt= document.getElementById('colortext');
+    const content = '<p>blue</p>';
+    txt.innerHTML=content;}
+);
+});
+const greenbox = document.querySelectorAll('.green');
+greenbox.forEach(function(box) {
+    box.addEventListener('click', function boxtext(){const txt= document.getElementById('colortext');
+    const content = '<p>green</p>';
+    txt.innerHTML=content;}
+);
+});
+
+const indigobox = document.querySelectorAll('.indigo');
+indigobox.forEach(function(box) {
+    box.addEventListener('click', function boxtext(){const txt= document.getElementById('colortext');
+    const content = '<p>violet</p>';
+    txt.innerHTML=content;}
+);
+});
+
+const violetbox = document.querySelectorAll('.violet');
+violetbox.forEach(function(box) {
+    box.addEventListener('click', function boxtext(){const txt= document.getElementById('colortext');
+    const content = '<p>indigo</p>';
+    txt.innerHTML=content;}
+    );
+});
 
