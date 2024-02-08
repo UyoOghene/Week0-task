86d8c70bde8480e0f47a2afbbc03f97efe857fd1
Uyooghene <uyooghene01@gmail.com>
2024-02-08 14:24:16 -0800

    class08

--- app.js
+++ app.js
@@ -1,67 +1,144 @@
+const colortext = document.getElementById('colortext');
+const uyo = document.getElementById('uyo');
 
-// const colors = [
-//     'red',
-//     'orange',
-//     'yellow',
-//     'green',
-//     'blue',
-//     'indigo',
-//     'violet'
-// ];
+function al(){
+    alert('hi')
+}
 
 
-const redbox = document.querySelectorAll('.red');
-redbox.forEach(function(box) {
-    box.addEventListener('click',function boxtext(){const txt= document.getElementById('colortext');
-    const content = '<p>red</p>';
-    txt.innerHTML=content;}
-);
-});
+// console.log('hi');
+function manos(){
+    console.log('red');
+    console.log(colortext.innerText);
+    colortext.innerText='red';
+}
+uyo.addEventListener('click', manos);
 
-const orangebox = document.querySelectorAll('.orange');
-orangebox.forEach(function(box) {
-    box.addEventListener('click', function boxtext(){const txt= document.getElementById('colortext');
-    const content = '<p>orange</p>';
-    txt.innerHTML=content;}
-);
-});
 
-const yellowbox = document.querySelectorAll('.yellow');
-yellowbox.forEach(function(box) {
-    box.addEventListener('click',function boxtext(){const txt= document.getElementById('colortext');
-    const content = '<p>yellow</p>';
-    txt.innerHTML=content;}
-);
-});
+// First target the element then declare as a variable
+const orange=document.getElementById('orange');
 
-const bluebox = document.querySelectorAll('.blue');
-bluebox.forEach(function(box) {
-    box.addEventListener('click', function boxtext(){const txt= document.getElementById('colortext');
-    const content = '<p>blue</p>';
-    txt.innerHTML=content;}
-);
-});
-const greenbox = document.querySelectorAll('.green');
-greenbox.forEach(function(box) {
-    box.addEventListener('click', function boxtext(){const txt= document.getElementById('colortext');
-    const content = '<p>green</p>';
-    txt.innerHTML=content;}
-);
-});
+// create a function to console log the color
+function corange(){
+    console.log(colortext.innerText);
+    colortext.innerText='orange';
+}
+// addEventListener to the targeted element
+orange.addEventListener('click',corange);
 
-const indigobox = document.querySelectorAll('.indigo');
-indigobox.forEach(function(box) {
-    box.addEventListener('click', function boxtext(){const txt= document.getElementById('colortext');
-    const content = '<p>violet</p>';
-    txt.innerHTML=content;}
-);
-});
+const yellow=document.getElementById('yellow');
+function cyellow(){
+    console.log(colortext.innerText);
+    colortext.innerText='yellow';
+}
+yellow.addEventListener('click',cyellow);
 
-const violetbox = document.querySelectorAll('.violet');
-violetbox.forEach(function(box) {
-    box.addEventListener('click', function boxtext(){const txt= document.getElementById('colortext');
-    const content = '<p>indigo</p>';
-    txt.innerHTML=content;}
-    );
-});
+
+const blue=document.getElementById('blue');
+function cblue(){
+    console.log(colortext.innerText);
+    colortext.innerText='blue';
+}
+blue.addEventListener('click',cblue);
+
+const green=document.getElementById('green');
+function cgreen(){
+    console.log(colortext.innerText);
+    colortext.innerText='green';
+}
+green.addEventListener('click',cgreen);
+
+const indigo=document.getElementById('indigo');
+function cindigo(){
+    console.log(colortext.innerText);
+    colortext.innerText='indigo';
+}
+indigo.addEventListener('click',cindigo);
+
+const violet=document.getElementById('violet');
+function cviolet(){
+    console.log(colortext.innerText);
+    colortext.innerText='violet';
+}
+violet.addEventListener('click',cviolet);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const redbox = document.querySelectorAll('.red');
+// redbox.forEach(function(box) {
+//     box.addEventListener('click',function boxtext(){const txt= document.getElementById('colortext');
+//     const content = '<p>red</p>';
+//     txt.innerHTML=content;}
+// );
+// });
+
+// const orangebox = document.querySelectorAll('.orange');
+// orangebox.forEach(function(box) {
+//     box.addEventListener('click', function boxtext(){const txt= document.getElementById('colortext');
+//     const content = '<p>orange</p>';
+//     txt.innerHTML=content;}
+// );
+// });
+
+// const yellowbox = document.querySelectorAll('.yellow');
+// yellowbox.forEach(function(box) {
+//     box.addEventListener('click',function boxtext(){const txt= document.getElementById('colortext');
+//     const content = '<p>yellow</p>';
+//     txt.innerHTML=content;}
+// );
+// });
+
+// const bluebox = document.querySelectorAll('.blue');
+// bluebox.forEach(function(box) {
+//     box.addEventListener('click', function boxtext(){const txt= document.getElementById('colortext');
+//     const content = '<p>blue</p>';
+//     txt.innerHTML=content;}
+// );
+// });
+// const greenbox = document.querySelectorAll('.green');
+// greenbox.forEach(function(box) {
+//     box.addEventListener('click', function boxtext(){const txt= document.getElementById('colortext');
+//     const content = '<p>green</p>';
+//     txt.innerHTML=content;}
+// );
+// });
+
+// const indigobox = document.querySelectorAll('.indigo');
+// indigobox.forEach(function(box) {
+//     box.addEventListener('click', function boxtext(){const txt= document.getElementById('colortext');
+//     const content = '<p>violet</p>';
+//     txt.innerHTML=content;}
+// );
+// });
+
+// const violetbox = document.querySelectorAll('.violet');
+// violetbox.forEach(function(box) {
+//     box.addEventListener('click', function boxtext(){const txt= document.getElementById('colortext');
+//     const content = '<p>indigo</p>';
+//     txt.innerHTML=content;}
+//     );
+// });
 
